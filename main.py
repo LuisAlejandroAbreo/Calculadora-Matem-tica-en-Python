ea97e4a1540f41e5a763c6ab0282d45c5dfe0a1b
LuisAlejandroAbreo <alejandro.abreo39@gmail.com>
2026-03-04 22:19:46 -0500

    feat: se agregan las funciones de los usuarios

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-from usuario_a import serie_fibonacci
+from usuario_a import serie_fibonacci, num_capicua, num_perfecto
+from usuario_b import calcular_factorial, verificar_primo, maximo_comun_divisor, numeros_primos   
+
+
