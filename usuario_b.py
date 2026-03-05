3e573d2903b389cc804f0226560545d1d3d2c440
Jesussanchez2805 <jesussanchez2805.js@gmail.com>
2026-03-04 20:44:35 -0500

    feat: se añade funcion factorial

--- usuario_b.py
+++ usuario_b.py
@@ -28,9 +28,10 @@ def verificar_primo():
         print(f"\n {n} NO es un número primo.")
     print("---------------------------------------")
 
-def factorial():
-    pass
-
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
 
 def maximo_comun_divisor():
     pass
